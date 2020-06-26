f47c080aee01a203b648c81282bbc6be2541568d
njacobs2019 <59352054+njacobs2019@users.noreply.github.com>
2020-06-26 11:52:56 -0400

    Update Configuration.h
    Update Babystepping probe offset value Z.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -955,7 +955,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 48, -2, -2 }
+#define NOZZLE_TO_PROBE_OFFSET { 48, -2, -3.32 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
